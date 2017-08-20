b669fcd2a1ce2488b90e279849bca1942ecc3712
Tempurturtul <tempurturtul@gmail.com>
2017-08-20 12:42:02 -0400

    Add boilerplate for server

---
+++ server/.eslintrc.js
@@ -0,0 +1,12 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: 'airbnb-base',
+  rules: {
+    "no-console": "off",
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const apiRouter = require('./routes/api');
+
+const app = express();
+
+// Set the port.
+app.set('port', process.env.PORT || 3000);
+
+// Use body-parser to parse forms and json in requests.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Static files.
+app.use(express.static('public'));
+
+// Routes.
+// app.use('/api/v1', apiRouter);
+
+// Start the server.
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}.`);
+});
