49bd103dadd939d61fdbe73c607af24bef374b1a
Tempurturtul <tempurturtul@gmail.com>
2017-12-19 06:56:57 -0500

    Update empty-db helper for new db structure

--- server/test/helpers/empty-db.js
+++ server/test/helpers/empty-db.js
@@ -1,8 +1,11 @@
 import db from '../../src/db/db';
 
 async function resetDB() {
-	await db.any('DELETE FROM blogPosts *');
+	await db.any('DELETE FROM blog_posts_tags *');
+	await db.any('DELETE FROM tags *');
+	await db.any('DELETE FROM blog_posts *');
 	await db.any('DELETE FROM users *');
+	await db.any('DELETE FROM roles *');
 }
 
 export default resetDB;
