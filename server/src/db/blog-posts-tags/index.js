f96584a132fc9c196c29fcabe969ce7b3a0812c6
Tempurturtul <tempurturtul@gmail.com>
2017-12-24 12:11:41 -0500

    Start db/blog-posts-tags module

---
+++ server/src/db/blog-posts-tags/index.js
@@ -0,0 +1,13 @@
+const create = require('./create-blog-post-tag');
+const get = require('./get-blog-post-tag');
+const getAll = require('./get-blog-posts-tags');
+const update = require('./update-blog-post-tag');
+const remove = require('./delete-blog-post-tag');
+
+module.exports = {
+	create,
+	get,
+	getAll,
+	update,
+	remove,
+};
